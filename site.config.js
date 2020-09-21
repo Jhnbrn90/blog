b272650d5668f61eddcc77a1c26670830f3eb4c9
John Braun <john@johnbraun.nl>
2020-09-21 22:01:12 +0200

    Extract the description for the about page

--- site.config.js
+++ site.config.js
@@ -2,6 +2,7 @@ export default {
     siteMetadata: {
         title: "John Braun's blog",
         description: "John Braun's weblog, mostly featuring articles Laravel, VueJS and programming related.",
+        aboutPage: "About John Braun's journey from a PhD in organic chemistry to working with Laravel, Vue and other modern technologies.",
         siteUrl: "https://johnbraun.blog",
         language: "en",
         social: {
