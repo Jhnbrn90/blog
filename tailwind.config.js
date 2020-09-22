899002564692e59d5513c182d0b783842ed25d22
John Braun <john@johnbraun.nl>
2020-09-22 16:08:09 +0200

    Update tailwind's purge to look in .tsx files instead of .js files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     purge: [
-        './pages/**/*.js',
-        './components/**/*.js',
+        './pages/**/*.tsx',
+        './components/**/*.tsx',
     ],
     future: {
         removeDeprecatedGapUtilities: true,
