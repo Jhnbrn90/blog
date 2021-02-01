34a9faeab6962632c8a7f0ffb320d04336631f4c
John Braun <john@brn.email>
2021-02-01 14:23:06 +0100

    Upgrade to NextJS 10

--- components/seo.tsx
+++ components/seo.tsx
@@ -14,7 +14,7 @@ export default function SEO({title, description, image}: SEOProps): JSX.Element 
         <Head>
             <title>{siteTitle}</title>
             <meta charSet='utf-8'/>
-            <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
+            <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
             <meta name='viewport'
                   content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no'/>
             <meta name='keywords' content=''/>
